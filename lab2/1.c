dc246009d2b7e4dc8aff5cffa5187c4a20cf4118
misterpetchz <petch.t1507@gmail.com>
2022-08-15 14:22:48 +0700

    lab2

--- lab2/1.c
+++ lab2/1.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 int main()
 {
-    int
+    int x,y=2;
+    scanf("%d" ,&x);
+    while (x!=1)
+    {
+        if (x%y == 0)
+        {
+            printf("%d" ,y);
+            x = x/y;
+            if (x!=1)
+            {
+                printf("x");
+            }
+        }
+        else 
+        {
+        y++;
+        }
 
-
+    }
     return 0;
 }

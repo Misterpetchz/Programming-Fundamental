2c4e3d496c7340c6cdd38529f669288bd5102e93
misterpetchz <petch.t1507@gmail.com>
2022-08-31 00:34:34 +0700

    lab4

--- lab3/3rd.c
+++ lab3/3rd.c
@@ -4,9 +4,9 @@ int main()
 {
     int n;
     scanf("%d",&n);
-    for(int i=0; i<=n; i++)
+    for(int i=1; i<=n; i++)
     {
-        for(int j=1; j <= 2*n-1; j++)
+        for(int j=1; j <= 2*n-1; j++) 
         {
             if(j<=i || j>=2*n-i)
             {

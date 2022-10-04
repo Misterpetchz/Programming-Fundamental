385ee7a6f5191b8f72ff6ed93011a3f7a59811a9
misterpetchz <petch.t1507@gmail.com>
2022-10-04 20:04:37 +0700

    fixed

--- weekly8/sqrt2.c
+++ weekly8/sqrt2.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
-#include <math.h>
+
+float sqrts (int number)
+{
+    float temp;
+    if (number >= 0)
+    {
+            for (temp = 0.001; temp * temp < number; temp += 0.001);
+            return temp;
+    }
+    else
+        return 0;
+}
 
 int main()
 {
     int number;
     scanf("%d" ,&number);
-    printf("%.2f" ,sqrt(number));
+    printf("%.2f" ,sqrts(number));
     return 0;
 }
